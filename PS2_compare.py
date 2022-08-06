a893dfd8242a5d8f9f6e7f69124cbb7d9d55049a
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-08-06 17:47:32 +0530

    Create PS2_compare.py

---
+++ PS2_compare.py
@@ -0,0 +1,12 @@
+def get_compare(val1,val2):
+  '''
+  val1 : input parameter as string.
+  val2 : input parameter as string.
+  returns 1 if val1>val2 , 0 if val1<val2
+  '''
+  return compare(val1,val2)
+  
+  
+ ##example:
+##get_compare('ac99','ab100')
+#output:  1
