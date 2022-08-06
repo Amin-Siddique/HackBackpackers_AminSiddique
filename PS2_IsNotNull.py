2af2b71fe3c9d7eb5dae5527ad5e6eeb34579626
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-08-06 17:02:46 +0530

    Create PS2_IsNotNull.py

---
+++ PS2_IsNotNull.py
@@ -0,0 +1,11 @@
+def get_IsNotNull(val):
+  '''
+  val : input parameter could be expression.
+  return is Boolean
+  '''
+  return isNull(val)
+  
+  
+ ##example:
+##get_IsNotNull(toString(null())
+#output:  True
