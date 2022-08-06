1282b22bc464029f3626592aaed6530cd65918e1
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-08-06 16:41:09 +0530

    Create PS2_BitAnd.py

---
+++ PS2_BitAnd.py
@@ -0,0 +1,10 @@
+def get_BitAnd(val1,val2):
+  '''
+  val1,val2 = 2 input parameters and both should be integer.
+  '''
+  retrun bitwiseAnd(val1,val2)
+
+  
+#example: 
+#get_BitAnd(352,400)
+#output=>256
