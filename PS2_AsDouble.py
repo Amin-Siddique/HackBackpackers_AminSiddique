b3670f9893ea02a5c4193bd1da48710fd975cdf3
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-08-06 17:12:13 +0530

    Create PS2_AsDouble.py

---
+++ PS2_AsDouble.py
@@ -0,0 +1,10 @@
+def get_AsDouble(val):
+  '''
+  val : input parameter could be expression
+  '''
+  return toDOuble(val)
+  
+  
+ ##example:
+##get_AsDouble(56/4.32)
+#output:  1.2962962962962962
