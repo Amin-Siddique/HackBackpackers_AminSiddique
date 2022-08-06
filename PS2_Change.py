f14978e09f16c40dcbf8bba60b55ef7602675d40
Amin Siddique <44993406+Amin-Siddique@users.noreply.github.com>
2022-08-06 17:39:47 +0530

    Create PS2_Change.py

---
+++ PS2_Change.py
@@ -0,0 +1,12 @@
+def get_Change(val1,val2,val3):
+  '''
+  val : input parameter as string.
+  val2 : value to be replaced
+  val3: value to be replaced by
+  '''
+  return replace(val1,val2,val3)
+  
+  
+ ##example:
+##get_Change('aaabbbcccbbb','bbb','ZZZ')
+#output:  aaaZZZcccZZZ
